6a8361c36624c4a4e22fb9d63bb5f3669876f271
waizonne <abhisit@ukham.com>
2022-10-01 14:29:57 +0700

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar">
+        <a href="/"> <i class="fas fa-user"></i> <span>profile</span> </a>
+        <a href="/about"> <i class="fas fa-address-card"></i> <span>about</span> </a>
+        <a href="/projects"> <i class="fas fa-briefcase"></i> <span>projects</span> </a>
+        <a href="/contact"> <i class="fas fa-address-book"></i> <span>contact</span> </a>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue"
+import About from "../pages/About.vue"
+
+const routes = [{
+    path: "/",
+    component: Home,
+    name: "Home",
+},{
+    path: "/about",
+    component: About,
+    name: "About",
+}]
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkActiveClass: "active",
+    scrollBehavior(to, from, savedPosition) {
+      if (to.hash) {
+        return {
+          selector: to.hash,
+          behavior: 'smooth'
+        };
+      }
+      return { x: 0, y: 0 };  // Go to the top of the page if no hash
+    },
+    routes,
+  });
+  
+  export default router;
